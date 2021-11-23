8b5d659bc98cba05e8ff17e1bccd0727752da2a1
mdelias <matthew.daniel.elias@gmail.com>
2021-11-23 10:18:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ import getPrefix from './lib/getPrefix';
 
 // Vars that will help us get er done
 const isDev = window.location.hostname === 'localhost';
-const speed = isDev ? 0 : 16;
+const speed = isDev ? 60 : 60;
 let style, styleEl, workEl, pgpEl, skipAnimationEl, pauseEl;
 let animationSkipped = false, done = false, paused = false;
 let browserPrefix;
@@ -52,6 +52,7 @@ async function startAnimation() {
 async function surprisinglyShortAttentionSpan() {
   if (done) return;
   done = true;
+  workEl.innerHTML = workText;
   pgpEl.innerHTML = pgpText;
   let txt = styleText.join('\n');
 
